54e2864f781457207fc96845a2c059c46a37b897
Ritik Panchal <44132205+ritikpanchal768@users.noreply.github.com>
2024-10-27 23:44:20 +0530

    Spring Initialization

---
+++ House-Hoot/src/test/java/com/example/House_Hoot/HouseHootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.House_Hoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
